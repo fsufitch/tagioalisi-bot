ae5167ab0650bbfa0cd283f4462576fda7dd29a0
Filip Sufitchi <fsufitchi@gmail.com>
2017-06-22 19:16:43 -0400

    Include /lib/ stuff

--- .profile.d/libs_patch.sh
+++ .profile.d/libs_patch.sh
@@ -1,2 +1,2 @@
-export LD_LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu/mesa:$LD_LIBRARY_PATH"
-export LIBRARY_PATH="$HOME/.apt/usr/lib/x86_64-linux-gnu/mesa:$LIBRARY_PATH"
+export LD_LIBRARY_PATH="$HOME/.apt/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/x86_64-linux-gnu/mesa:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="$HOME/.apt/lib/x86_64-linux-gnu:$HOME/.apt/usr/lib/x86_64-linux-gnu/mesa:$LIBRARY_PATH"
