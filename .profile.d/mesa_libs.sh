de4a3e5abf17dca20098a442768dc2b5ade03738
Filip Sufitchi <fsufitchi@gmail.com>
2017-06-22 18:59:53 -0400

    Add mesa libs hardcode

---
+++ .profile.d/mesa_libs.sh
@@ -0,0 +1,3 @@
+export LD_LIBRARY_PATH="$HOME/.apt/usr/lib/i386-linux-gnu/mesa:$LD_LIBRARY_PATH"
+export LIBRARY_PATH="$HOME/.apt/usr/lib/i386-linux-gnu/mesa:$LIBRARY_PATH"
+export INCLUDE_PATH="$HOME/.apt/usr/include/x86_64-linux-gnu/mesa:$INCLUDE_PATH"
