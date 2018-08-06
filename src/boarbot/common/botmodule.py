84b4ef9f7481f4d66c291aef41962bab7b397b8a
Filip Sufitchi <fsufitchi@gmail.com>
2018-08-06 16:34:51 -0400

    don't flood with shlex failures

--- src/boarbot/common/botmodule.py
+++ src/boarbot/common/botmodule.py
@@ -34,7 +34,7 @@ class BotModule(metaclass=ABCMeta):
         try:
             parts = shlex.split(content) # type: [str]
         except Exception as e:
-            LOGGER.info('Failed shlex.split on ' + str(content))
+            LOGGER.debug('Failed shlex.split on ' + str(content))
             return None
 
         if not parts or parts[0] != command:
