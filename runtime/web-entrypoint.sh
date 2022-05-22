65955f1571680f12bb7d341a597ffeae67b25d91
Filip Sufitchi <fsufitchi@gmail.com>
2022-05-22 21:32:22 +0000

    Even better runtime variable speccing

--- runtime/web-entrypoint.sh
+++ runtime/web-entrypoint.sh
@@ -5,6 +5,7 @@ set -x;
 # Create a config using environment vars to substitute stuff in it, since Nginx can't do that natively
 cat ./tagioalisi.nginx_template.conf \
     | sed "s#%%BOT_EXTERNAL_BASE_URL%%#${BOT_EXTERNAL_BASE_URL}#" \
+    | sed "s#%%NGINX_PORT%%#${NGINX_PORT}#" \
     | tee /etc/nginx/conf.d/tagioalisi.conf
 
 # Command from original nginx:alpine image
