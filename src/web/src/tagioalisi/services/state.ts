4872459ded408b829a1e68bca3cd0af61e9e84c4
Filip Sufitchi <fsufitchi@gmail.com>
2022-05-25 02:59:43 +0000

    Rework state to be truly cross-tab

--- src/web/src/tagioalisi/services/state.ts
+++ src/web/src/tagioalisi/services/state.ts
@@ -1,89 +1,89 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 export type Serializer<T> = (data: T) => string
 export type Deserializer<T> = (raw: string) => T
 
 
 class SynchronizedState {
-    private keyUpdateCallbacks: {[key: string]: Set<() => void>} = {}
+    constructor(public readonly storageKeyPrefix: string, public readonly storage: Storage) {}
 
-    constructor(public readonly storageKeyPrefix: string) {
-        window.addEventListener('storage', (ev) => this.handleStorageEvent(ev));
-    }
-
-    private handleStorageEvent(event: StorageEvent) {
-        if (!event.key?.startsWith(this.storageKeyPrefix)) {
-            // Only handle events actually relevant to our interests
-            return;
-        }
-        const key = event.key.slice(this.storageKeyPrefix.length);
-        if (!key) {
-            // Weird edge case
-            console.error('Empty storage event key after prefix strip', event);
-            return;
-        }
-    }
-
-    private notifyKey(key: string) {
-        // Call the callbacks for this key; async, since who knows how heavy they might be
-        const callbacks = this.keyUpdateCallbacks[key] ?? [];
-        for (const cb of callbacks) {
-            Promise.resolve().then(cb);
-        }
-    }
-
-    useSynchronizedState<T>(key: string, defaultValue: T, serialize: Serializer<T>, deserialize: Deserializer<T>) {
+    use<T>(key: string, defaultValue: T, serialize: Serializer<T>, deserialize: Deserializer<T>) {
         const storageKey = this.storageKeyPrefix + key;
-        let value: T;
 
         // Initialize the localStorage entry if necessary
-        const existingValue = localStorage.getItem(storageKey)
-        if (existingValue == null) {
-            value = defaultValue;
-            localStorage.setItem(storageKey, serialize(value));
-        } else {
-            value = deserialize(existingValue);
-        }
-
-        const setter = (newValue: T) => {
-            value = newValue;
-            localStorage.setItem(storageKey, serialize(newValue));
-            this.notifyKey(key);
-        }
-
-        const notifyKeyCallback = () => {
-            const rawValue = localStorage.getItem(storageKey);
-            if (rawValue == null) {
-                console.warn(`Received update for key '${key}' but value was missing; using default`);
-                value = defaultValue;
-            } else {
-                value = deserialize(rawValue);
+        let defaultStateValue: T = defaultValue;
+        const initialStorageValue = localStorage.getItem(storageKey);
+        if (initialStorageValue != null) {
+            try {
+                defaultStateValue = deserialize(initialStorageValue);
+            } catch (err) {
+                console.error(`Could not load initial value for key '${key}' from storage`, err);
+                localStorage.setItem(key, serialize(defaultValue));
             }
         }
-
+        
+        const [value, setValue] = useState<T>(defaultStateValue);
+        // Async load value from storage events
         useEffect(() => {
-            // Register the update notify callback, and a cleanup
-            this.keyUpdateCallbacks[key] ??= new Set();
-            this.keyUpdateCallbacks[key].add(notifyKeyCallback);
+            const cb = (ev: StorageEvent) => {
+                if (ev.key != storageKey) {
+                    return;
+                }
+                let newValue = localStorage.getItem(storageKey);
+                if (newValue == null) {
+                    console.log(`After storage update, key '${key}' contained null`);
+                    setValue(defaultValue);
+                    return;
+                }
+                try {
+                    setValue(deserialize(newValue));
+                } catch (ex) {
+                    console.error(`After update, key ${key} had invalid value`, newValue, ex);
+                    setValue(defaultValue);
+                }
+            };
+            window.addEventListener('storage', cb);
+            // Deregister the callback when the user component is unloaded
             return () => {
-                this.keyUpdateCallbacks[key].delete(notifyKeyCallback);
-            }
+                window.removeEventListener('storage', cb);
+            };
         }, []);
 
-        return [value, setter] as [T, (newValue: T) => void];
+        // writeValue ephemerally contains (and triggers) writing values to localstorage
+        const [writeValue, setWriteValue] = useState<T | null>(null);
+        useEffect(() => {
+            if (writeValue == null) return;
+            setWriteValue(null);
+            localStorage.setItem(storageKey, serialize(writeValue));
+            setValue(writeValue); // Need to also manually update it here, since storage events only happen in other documents
+        }, [writeValue]);
+
+        // removeValue ephemerally contains a trigger to delete stuff from localstorage
+        const [removeValue, setRemoveValue] = useState<boolean>(false);
+        useEffect(() => {
+            if (!removeValue) return;
+            setRemoveValue(false);
+            localStorage.removeItem(storageKey);
+            setValue(defaultValue); // Need to also manually update it here, since storage events only happen in other documents
+        }, [writeValue]);
+
+
+        return [value, setWriteValue, () => setRemoveValue(true)] as [T, (newValue: T) => void, () => void];
+
+
     }
 
     // Static below
 
     private static _instance?: SynchronizedState;
     static get instance() {
-        this._instance = this._instance ?? new SynchronizedState('sync-state::');
+        this._instance = this._instance ?? new SynchronizedState('sync-state::', sessionStorage);
         return this._instance;
     }
 }
 
 
 export const useSynchronizedState = <T>(key: string, defaultValue: T, serialize: Serializer<T>, deserialize: Deserializer<T>) => 
-    SynchronizedState.instance.useSynchronizedState(key, defaultValue, serialize, deserialize);
+    SynchronizedState.instance.use(key, defaultValue, serialize, deserialize);
 
 // Convenience JSON wrapepr
 
