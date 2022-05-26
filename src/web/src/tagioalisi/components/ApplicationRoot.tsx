cc783890de9999d7c6345c0dd0c88f6ae18a7604
Filip Sufitchi <fsufitchi@gmail.com>
2022-05-26 01:28:51 +0000

    Enable dark mode

--- src/web/src/tagioalisi/components/ApplicationRoot.tsx
+++ src/web/src/tagioalisi/components/ApplicationRoot.tsx
@@ -2,15 +2,25 @@ import React from "react";
 
 import { Container } from "@mui/material";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import ApplicationBar from "tagioalisi/components/ApplicationBar";
 import TagioalisiRoutes from 'tagioalisi/components/TagioalisiRoutes';
+import { useTheme } from 'tagioalisi/services/theme';
 
 
-export default () =>
-  <Router>
-    <Container maxWidth="md">
-      <ApplicationBar />
-      <TagioalisiRoutes />
-    </Container>
-  </Router>
+export default () => {
+  const theme = useTheme();
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Container maxWidth="md">
+          <ApplicationBar />
+          <TagioalisiRoutes />
+        </Container>
+      </Router>
+    </ThemeProvider>
+  );
+}
