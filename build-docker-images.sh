4b2df9752d914537376ac3a9f43d067163b81510
Filip Sufitchi <fsufitchi@gmail.com>
2023-02-14 17:37:39 -0500

    Add builder script and use images for runtime compose

---
+++ build-docker-images.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker build -f Dockerfile.discordbot -t docker.io/fsufitch/tagioalisi-discordbot .
+docker build -f Dockerfile.webapp -t docker.io/fsufitch/tagioalisi-webapp .
+docker build -f Dockerfile.grpcwebproxy -t docker.io/fsufitch/tagioalisi-grpcwebproxy .
+
