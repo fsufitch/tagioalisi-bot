942117c137654db936ebe6aa957f15c9c4847bb9
Dallas Womack <dallasawomack@gmail.com>
2023-03-20 09:18:33 -0700

    removed test that was supplying an invalid api key w/o mocking

--- discordbot/merriam-webster/mwdict_test.go
+++ discordbot/merriam-webster/mwdict_test.go
@@ -18,14 +18,3 @@ func TestBasicClientErrorMessagesNoAPIKey(t *testing.T) {
 	// Asserts
 	assert.Equal(t, target_error, err)
 }
-
-func TestBasicClientErrorMessagesKeyProvided(t *testing.T) {
-	// Setup
-	bc := NewBasicClient("api_key", "testuseragent")
-
-	// Tested code
-	_, _, err := bc.SearchCollegiate("testing")
-
-	// Asserts
-	assert.Equal(t, nil, err)
-}
