3beea9aaf275dc94bc3de155db8131d0f9742a28
Filip Sufitchi <Filip Sufitchi>
2024-02-15 21:39:22 -0500

    Include maximum result count

--- discordbot/azure/newssearch.go
+++ discordbot/azure/newssearch.go
@@ -41,6 +41,7 @@ func (a BingNewsSearch) Search(ctx context.Context, query string, maxNum int32) 
 	if maxNum > 10 || maxNum < 1 {
 		return nil, errors.New("article count must be between 1 and 10")
 	}
+	q.Set("count", fmt.Sprintf("%d", maxNum))
 	
 	url.RawQuery = q.Encode()
 
